e000756ca37478e65716a91fa0748068104afa90
M Saeed Shaikh <g555066@genmills.com>
2020-05-26 09:37:15 -0500

    test

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Configure the Alicloud Provider
 
 provider "alicloud" {
-  access_key = var.AccessKeyId
-  secret_key = var.AccessKeySecret
+#  access_key = var.AccessKeyId
+#  secret_key = var.AccessKeySecret
   region = var.region
 }
 
 module "vpc_module" {
-  source = "source = "git::https://github.com/saeedsurat/terraform-alicloud-module-vpc"
+  source = "git::https://github.com/saeedsurat/terraform-alicloud-module-vpc"
   #source = "/home/g555066/terraform/SAEED-ALI/learn-modules/module/vpc"
   vpc_cidr =  "10.0.0.0/16"
   zones = ["ap-south-1a","ap-south-1b"]
--- variable.tf
+++ variable.tf
@@ -1,10 +1,3 @@
-variable "AccessKeySecret"{
-   default = "ju7g6HW3VsoGxx5YfvCTBXFRpRcrAY"
-}
-
-variable "AccessKeyId"{
-   default = "LTAI4G54aTaBx6GH4zphpUnm"
-}
 
 variable "region"{
    default = "ap-south-1"
