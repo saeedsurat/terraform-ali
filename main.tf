23471834009c35058268c5f9660c47f462365929
M Saeed Shaikh <g555066@genmills.com>
2020-05-26 08:52:30 -0500

    Test

---
+++ main.tf
@@ -0,0 +1,16 @@
+# Configure the Alicloud Provider
+
+provider "alicloud" {
+  access_key = var.AccessKeyId
+  secret_key = var.AccessKeySecret
+  region = var.region
+}
+
+module "vpc_module" {
+  source = "/home/g555066/terraform/SAEED-ALI/learn-modules/module/vpc"
+  vpc_cidr =  "10.0.0.0/16"
+  zones = ["ap-south-1a","ap-south-1b"]
+  vswitch_cidr = ["10.0.1.0/24","10.0.2.0/24"]
+}
+
+
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "AccessKeySecret"{
+   default = "ju7g6HW3VsoGxx5YfvCTBXFRpRcrAY"
+}
+
+variable "AccessKeyId"{
+   default = "LTAI4G54aTaBx6GH4zphpUnm"
+}
+
+variable "region"{
+   default = "ap-south-1"
+}   
+
