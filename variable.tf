13c680a29a9ba946a81cd123ce7e52f3f0eb34f7
M Saeed Shaikh <g555066@genmills.com>
2020-05-31 07:32:48 -0500

    test

--- variable.tf
+++ variable.tf
@@ -3,14 +3,21 @@ variable "region"{
    default = "ap-south-1"
 }   
 
+  #vpc_cidr =  "10.0.0.0/16"
+  #zones = ["ap-south-1a","ap-south-1b"]
+  #vswitch_cidr = ["10.0.1.0/24","10.0.2.0/24"]
+
 variable "vpc_cidr"{
+  default = "10.0.0.0/16"
 }
 
 variable "zones" {
    type = list
+   default = ["ap-south-1a","ap-south-1b"]
 }
 
 variable "vswitch_cidr"{
    type = list
+   default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 
