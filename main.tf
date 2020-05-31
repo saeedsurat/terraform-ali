b2aa5fac8ac3dfad4aebef48f4b6809c829119a4
M Saeed Shaikh <g555066@genmills.com>
2020-05-31 06:57:55 -0500

    test

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ module "vpc_module" {
   #source = "git::https://github.com/saeedsurat/terraform-alicloud-module-vpc"
   #source = "/home/g555066/terraform/SAEED-ALI/learn-modules/module/vpc"
   source  = "app.terraform.io/saeed-terraform-cloud-learning/module-vpc/alicloud"
-  #vpc_cidr =  "10.0.0.0/16"
-  #zones = ["ap-south-1a","ap-south-1b"]
-  #vswitch_cidr = ["10.0.1.0/24","10.0.2.0/24"]
+  vpc_cidr =  "10.0.0.0/16"
+  zones = ["ap-south-1a","ap-south-1b"]
+  vswitch_cidr = ["10.0.1.0/24","10.0.2.0/24"]
 }
 
 
